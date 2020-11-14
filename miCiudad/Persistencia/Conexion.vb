d4569c80a1bb3d71be151f725ddf43d7ed664ac5
LuciaAnza <49279772+LuciaAnza@users.noreply.github.com>
2020-11-13 21:26:29 -0300

    terminado

--- miCiudad/Persistencia/Conexion.vb
+++ miCiudad/Persistencia/Conexion.vb
@@ -5,10 +5,15 @@
         Try
             conexion = New Npgsql.NpgsqlConnection
             Dim cadenaConexion As String
+<<<<<<< Updated upstream
 
             'cadenaConexion = "Server=127.0.0.1;Port=5432;User Id='postgres';Password='root';Database =miCiudad"
             cadenaConexion = "Server=10.1.14.80;Port=5432;User Id='postgres';Password='password';Database =miciudad"
 
+=======
+            'cadenaConexion = "Server=127.0.0.1;Port=5432;User Id='postgres';Password='root';Database =miCiudad"
+            cadenaConexion = "Server=10.1.14.80;Port=5432;User Id='postgres';Password='password';Database =miCiudad"
+>>>>>>> Stashed changes
             conexion.ConnectionString = cadenaConexion
             conexion.Open()
 
